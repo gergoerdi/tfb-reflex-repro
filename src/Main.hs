c4345612bd5f1420ddf7eaadb375c0d31a844843
Dr. ERDI Gergo <gergo@erdi.hu>
2016-03-11 16:54:33 +0800

    SVG animation

--- src/Main.hs
+++ src/Main.hs
@@ -1,17 +1,38 @@
 {-# LANGUAGE RecursiveDo #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 import Reflex.Dom
 import Reflex.Dom.Contrib.Widgets.Svg
 import Data.Time.Clock
 import Control.Monad.Trans
+import Data.Map (Map)
+import qualified Data.Map as Map
 
 main = mainWidget $ do
     now <- liftIO getCurrentTime
-    tick <- tickLossy 1 now -- clock that ticks once every so often
-    rec wealth <- foldDyn (+) 0 (mergeWith (+) [1 <$ click, (-10) <$ buyDrone, droneProd])
-        el "div" $ display wealth
-        click <- button "Click"
-        buyDrone <- fmap (gate (fmap (>= 10) (current wealth))) $ button "Buy Drone (10)"
-        drones <- foldDyn (+) 0 (1 <$ buyDrone)
-        let droneProd = tagDyn drones tick
+    tick <- tickLossy 0.1 now -- clock that ticks once every so often
+    time <- mapDyn fst =<< foldDyn (const $ uncurry keepTime) (0, True) tick
+    cx <- mapDyn (* 5) time
+    fullSVG $ do
+        svgAttr "rect" (Map.fromList [("fill", "lime"),("width", "100%"),("height", "100%")]) $ return ()
+        let toAttrs cx = Map.fromList [ ("cx", show cx)
+                                      , ("cy", "50")
+                                      , ("r", "40")
+                                      , ("stroke", "green")
+                                      , ("width", "4")
+                                      , ("fill", "yellow")
+                                      ]
+        attrs <- mapDyn toAttrs cx
+        svgDynAttr "circle" attrs $ return ()
     return ()
+
+fullSVG :: forall t m a. MonadWidget t m => m a -> m a
+fullSVG = svgAttr "svg" $ Map.fromList [ ("width", "100%")
+                                       , ("height", "100%")
+                                       ]
+
+keepTime :: Int -> Bool -> (Int, Bool)
+keepTime x True | x < 20 = (x + 1, True)
+                | otherwise = (x - 1, False)
+keepTime x False | x > 0 = (x - 1, False)
+                 | otherwise = (x + 1, True)
