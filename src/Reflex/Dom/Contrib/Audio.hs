364bf3f9cbe9aab1e6122fde7f4201871d6cc19a
Dr. ERDI Gergo <gergo@erdi.hu>
2016-03-12 14:19:01 +0800

    Use existing GHCJS-DOM functions for implementing `audio` commands

--- src/Reflex/Dom/Contrib/Audio.hs
+++ src/Reflex/Dom/Contrib/Audio.hs
@@ -12,7 +12,7 @@ import Data.Functor (($>))
 
 import GHCJS.Types
 import Data.String (IsString(..))
-import GHCJS.DOM.HTMLMediaElement
+import GHCJS.DOM.HTMLMediaElement as Media
 
 data AudioCommand = Play
                   | Pause
@@ -22,27 +22,14 @@ data AudioCommand = Play
 foreign import javascript unsafe "new Audio($1)"
     createAudio :: JSString -> IO HTMLMediaElement
 
-foreign import javascript unsafe "$1.play()"
-    playMedia :: HTMLMediaElement -> IO ()
-
-foreign import javascript unsafe "$1.pause()"
-    pauseMedia :: HTMLMediaElement -> IO ()
-
-foreign import javascript unsafe "$1.currentTime = $2"
-    seekMedia :: HTMLMediaElement -> Double -> IO ()
-
-foreign import javascript unsafe "$1.currentTime"
-    getMediaPos :: HTMLMediaElement -> IO Double
-
 audio :: MonadWidget t m => String -> Event t () -> Dynamic t AudioCommand -> m (Event t Double)
 audio path sampler cmd = do
     player <- liftIO $ createAudio (fromString path)
     let process cmd = case cmd of
-            Play -> liftIO $ playMedia player
-            Pause -> liftIO $ pauseMedia player
-            Seek timestamp -> liftIO $ seekMedia player timestamp
+            Play -> Media.play player
+            Pause -> Media.pause player
+            Seek timestamp -> Media.setCurrentTime player timestamp
     schedulePostBuild $ do
         process =<< sample (current cmd)
     addVoidAction $ fmap process (updated cmd)
-    performEvent $ sampler $> do
-        liftIO $ getMediaPos player
+    performEvent $ sampler $> Media.getCurrentTime player
